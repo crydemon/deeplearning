bfae3cbb87fccd22605aa86ba037642ae19cb31b
wq <wqkant@gmail.com>
2019-03-19 10:13:30 +0800

    add p1.3

--- foo/sicp/chapter1/p1.3.py
+++ foo/sicp/chapter1/p1.3.py
@@ -1,13 +1,23 @@
-def sqrt(x, precsion):
-    cur_precsion = 1
+def sqrt(x, precsion, guess):
+    cur_precsion = 1.0
     d = 1.0
     while cur_precsion >= precsion:
-        q = x / d
-        avg = (q + d) / 2
-        cur_precsion = abs(d - q)
-        d = avg
+        d, cur_precsion = guess(x, d)
     return d
 
 
+def guess2(x, y):
+    g = x * 1.0 / y
+    b = (g + y) / 2
+    return b, abs(g - b)
+
+
+def guess3(x, y):
+    g = x * 1.0 / (y * y)
+    b = (x / (y * y) + 2 * y) / 3
+    return b, abs(g - b)
+
+
 if __name__ == '__main__':
-    print(sqrt(2, 0.00000001))
+    print(sqrt(9, 0.00000001, guess2))
+    print(sqrt(9, 0.00000001, guess3))
