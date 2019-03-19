2e5181f4fd6b2e5a5154ed2c27da8d8c51b40337
wq <wqkant@gmail.com>
2019-03-19 18:04:16 +0800

    add comment

--- foo/sicp/chapter1/p1.3.py
+++ foo/sicp/chapter1/p1.3.py
@@ -121,6 +121,10 @@ def triangle(n):
             xs.append(recursive(i, j))
         print(xs)
 
+# yield在函数中的功能类似于return，不同的是yield每次返回结果之后函数并没有退出，
+# 而是每次遇到yield关键字后返回相应结果，并保留函数当前的运行状态，等待下一次的调用。
+# 如果一个函数需要多次循环执行一个动作，并且每次执行的结果都是需要的，这种场景很适合使用yield实现。
+# 包含yield的函数成为一个生成器，生成器同时也是一个迭代器，支持通过next方法获取下一个值。
 
 def triangles1(n1):
     def answer():
