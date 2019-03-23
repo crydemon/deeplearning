cb86973eea1279b371f32f9dad0d80c751408edf
wq <wqkant@gmail.com>
2019-03-23 11:08:11 +0800

    fix bug

--- foo/sicp/chapter2/p2.1.py
+++ foo/sicp/chapter2/p2.1.py
@@ -24,3 +24,48 @@ def div_rat(x, y):
 
 def equal_rat(x, y):
     return numer(x) * denom(y) == denom(x) * numer(y)
+
+
+def print_rat(x):
+    print()
+    display(numer(x))
+    display("/")
+    display(denom(x))
+
+
+def display(x):
+    print(x, end='')
+
+
+def make_rat(n, d):
+    return [n, d]
+
+
+def numer(x):
+    return car(x)
+
+
+def denom(x):
+    return cdr(x)
+
+
+def car(x):
+    return x[0]
+
+
+def cdr(x):
+    return x[1]
+
+
+def one_half():
+    return make_rat(1, 2)
+
+
+def one_third():
+    return make_rat(1, 3)
+
+
+if __name__ == '__main__':
+    print_rat(one_half())
+    print_rat(one_third())
+    print_rat(add_rat(one_half(), one_third()))
