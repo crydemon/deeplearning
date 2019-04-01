565e1301bbf6644531fee87392d32b9fa3c60e91
wqkant <wqkant@gmail.com>
2019-04-02 00:14:59 +0800

    add ch5

---
+++ foo/meachinglearning/chapter5/logRegres.py
@@ -0,0 +1,66 @@
+import numpy as np
+import math
+
+
+def loadDataSet():
+    dataMat = []                                                        #创建数据列表
+    labelMat = []                                                        #创建标签列表
+    fr = open('testSet.txt')                                            #打开文件
+    for line in fr.readlines():                                            #逐行读取
+        lineArr = line.strip().split()                                    #去回车，放入列表
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])        #添加数据
+        labelMat.append(int(lineArr[2]))                                #添加标签
+    fr.close()                                                            #关闭文件
+    return dataMat, labelMat                                            #返回
+
+
+def sigmoid(inX):
+    return 1 / (1 + np.exp(-inX))
+
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = np.mat(dataMatIn)                                        #转换成numpy的mat
+    labelMat = np.mat(classLabels).transpose()                            #转换成numpy的mat,并进行转置
+    m, n = np.shape(dataMatrix)                                            #返回dataMatrix的大小。m为行数,n为列数。
+    alpha = 0.001                                                        #移动步长,也就是学习速率,控制更新的幅度。
+    maxCycles = 1000                                                        #最大迭代次数
+    weights = np.ones((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix * weights)                                #梯度上升矢量化公式
+        error = labelMat - h
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return weights.getA()                                                #将矩阵转换为数组，返回权重数组
+
+
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n = np.shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1');
+    plt.ylabel('X2');
+    plt.show()
+
+
+if __name__ == '__main__':
+    dataMat, labelMat = loadDataSet()
+    weights = gradAscent(dataMat, labelMat)
+    plotBestFit(weights)
