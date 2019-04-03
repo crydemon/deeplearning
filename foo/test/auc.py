5427feafa584017146bbf7ebecd4ab09b4a6fd27
wqkant <wqkant@gmail.com>
2019-04-04 01:16:46 +0800

    add auc

--- foo/test/auc.py
+++ foo/test/auc.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.metrics import roc_auc_score
-
+# AUC的物理意义为任取一对例和负例，正例得分大于负例得分的概率，AUC越大，表明方法效果越好。
 if __name__ == '__main__':
     y_true = np.array([1, 1, 0, 0, 1, 1, 0])
     y_scores = np.array([0.8, 0.7, 0.5, 0.5, 0.5, 0.5, 0.3])
