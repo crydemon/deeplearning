89f45290c3720fbfb44bbfa216248030642ab682
wqkant <wqkant@gmail.com>
2019-04-07 22:19:08 +0800

    add crawl

---
+++ foo/crawl/s1.py
@@ -0,0 +1,34 @@
+import requests
+import pdfkit
+import os
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    target = 'https://blog.csdn.net/dpengwang/article/details/88355744'
+    req = requests.get(url=target)
+    html = req.text
+    bf = BeautifulSoup(html)
+    content = bf.find_all('article')[0]
+    # 渲染的html模板
+    html_template = """
+       <!DOCTYPE html>
+       <html lang="en">
+       <head>
+           <meta charset="UTF-8">
+       </head>
+       <body>
+       {content}
+       </body>
+       </html>
+       """
+    html = html_template.format(content=content)
+    html = html.encode("UTF-8")
+    out_file = r'f:\out.html'
+    fw = open(out_file, 'wb')
+    fw.write(html)
+    fw.close()
+
+    config = pdfkit.configuration(wkhtmltopdf=r'F:\apps\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    pdfkit.from_url(out_file, r'f:\out.pdf', configuration=config)
+    os.remove(out_file)
+    print("done")
