cc63f9d653073a49dd6b36b5f9eb6db8569705eb
wq <wqkant@gmail.com>
2019-04-18 19:35:22 +0800

    string

--- foo/dive-into-py/derivation.py
+++ foo/dive-into-py/derivation.py
@@ -12,7 +12,15 @@ if __name__ == '__main__':
     print((shortname, extension))
     os.chdir(dirname)
     print(glob.glob('*/*.py'))
+
     metadata = os.stat(os.getcwd())
     print(os.getcwd())
     print(metadata)
     print(os.path.realpath('derivation.py'))
+
+    print([i * i for i in range(5)])
+    metadata_dict = {f: os.stat(f) for f in glob.glob('*/*.py')}
+    print(metadata_dict)
+    print(metadata_dict.keys())
+    a_dict = {'a': 1, 'b': 2, 'c': 3}
+    print({value: key for key, value in a_dict.items()})
---
+++ foo/dive-into-py/strings.py
@@ -0,0 +1,5 @@
+# 在Python 3，所有的字符串都是使用Unicode编码的字符序列
+
+if __name__ == '__main__':
+    s = '深入'
+    print(len(s))
